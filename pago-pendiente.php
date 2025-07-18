84f2ecf8ea69c5c1106488f1499490726e0a4cfc
salvadorjns <chavasalvador.jns@gmail.com>
2025-07-18 07:42:50 -0600

    Create pago-pendiente.php

---
+++ pago-pendiente.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pago Pendiente</title>
+</head>
+<body>
+    <h1>Pago Pendiente</h1>
+    <p>Tu pago está siendo procesado. Te notificaremos cuando sea aprobado.</p>
+    <a href="/">Volver al inicio</a>
+</body>
+</html>
