d0eb017f48846953b1bba0479722cae8176680a2
salvadorjns <chavasalvador.jns@gmail.com>
2025-07-18 07:42:25 -0600

    Create pago-fallido.php

---
+++ pago-fallido.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pago Fallido</title>
+</head>
+<body>
+    <h1>Pago No Completado</h1>
+    <p>El pago no pudo ser procesado. Por favor intenta nuevamente.</p>
+    <a href="/carrito.php">Volver al carrito</a>
+</body>
+</html>
